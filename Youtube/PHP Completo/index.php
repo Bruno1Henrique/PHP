1898730ac1acb3b4eed142925967a8c97161a412
Bruno Henrique <bruno_henrique_pedroso@hotmail.com>
2022-05-26 20:26:30 -0300

    Adicao

--- Youtube/PHP Completo/index.php
+++ Youtube/PHP Completo/index.php
@@ -1,1 +1,16 @@
 <?php
+    $html = "<h1>Ola mundo!</h1><p>Olha que legal</p>"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo $html; ?>
+</body>
+</html>
