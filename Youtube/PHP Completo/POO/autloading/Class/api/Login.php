b8261422b98fbde8cd4877eee27bfe15cf25731b
Bruno Henrique <bruno_henrique_pedroso@hotmail.com>
2022-06-13 21:40:44 -0300

    continuando

---
+++ Youtube/PHP Completo/POO/autloading/Class/api/Login.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace api;
+
+class Login{
+    function __construct()
+    {
+        echo "Classe login Google inicializada";
+    }
+}
+
+class Cadastrar{
+    function __construct()
+    {
+        echo "Classe cadastrar Google inicializada";
+    }
+}
---
+++ Youtube/PHP Completo/POO/autloading/Class/src/login.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace src;
+
+class Login{
+    function __construct()
+    {
+        echo "Classe login inicializada";
+    }
+}
+
+class Cadastrar{
+    function __construct()
+    {
+        echo "Classe cadastrar inicializada";
+    }
+}
